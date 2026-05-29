a782f57d02956126062f6b826bdbe50ea65a134e
Hady Zen'in <hadyzenin@gmail.com>
2026-05-29 23:09:52 +0700

    Hady Zen'in

--- perintah/status.js
+++ perintah/status.js
@@ -9,6 +9,6 @@ module.exports = {
   
   Ayanokoji: async function ({ api, event, getData }) {
     const { nama, level, exp, yen } = getData(event.senderID);
-    api.sendMessage(`🜲 𝗦𝘁𝗮𝘁𝘂𝘀\n\nNama: ${nama}\nId: ${event.senderID}\nYen: ${yen}¥\nLevel: ${level}`, event.threadID, event.messageID);
+    api.sendMessage(`𝗦𝘁𝗮𝘁𝘂𝘀\n\nNama: ${nama}\nId: ${event.senderID}\nYen: ${yen}¥\nLevel: ${level}`, event.threadID, event.messageID);
   }
 };
