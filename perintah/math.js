a5fc07743f28054baa46ad94641e6536b0b89e82
Hady Zen'in <hadyzenin@gmail.com>
2026-05-29 23:01:28 +0700

    Hady Zen'in

--- perintah/math.js
+++ perintah/math.js
@@ -1,27 +1,49 @@
-const axios = require('axios');
+const axios = require("axios");
+
 module.exports = {
-  hady: { 
-    nama: "math", 
-    penulis: "Hady Zen", 
+  hady: {
+    nama: "math",
+    penulis: "Hady Zen",
     kuldown: 10,
     peran: 0,
     tutor: "<soal>"
-  }, 
+  },
 
-  bahasa: {
-    id: { hadi: "Kamu belum memberikan soal nya." }, 
-    en: { hadi: "You haven't given her soal yet." }
-  }, 
-  
-  Ayanokoji: async function ({ api, event, args, bhs }) {
-   try { 
-  if (!args.join(' ')) api.sendMessage(bhs('hadi'), event.threadID, event.messageID);
-      
-    const jumlah = await axios.get(`http://api.mathjs.org/v4/?expr=${encodeURIComponent(args.join(' '))}`);
-  api.sendMessage(args.join(' ') + " = " + jumlah.data, event.threadID, event.messageID);
-     
-} catch (error) {
-  api.sendMessage('Error: ' + error.message, event.threadID, event.messageID)
-}
- }
+  Ayanokoji: async function ({ api, event, args }) {
+
+    if (!args[0]) {
+
+      return api.sendMessage(
+        "Kamu belum memberikan soal nya.",
+        event.threadID,
+        event.messageID
+      );
+
+    }
+
+    try {
+
+      const soal = args.join(" ");
+
+      const jumlah = await axios.get(
+        `http://api.mathjs.org/v4/?expr=${encodeURIComponent(soal)}`
+      );
+
+      api.sendMessage(
+        `${soal} = ${jumlah.data}`,
+        event.threadID,
+        event.messageID
+      );
+
+    } catch (error) {
+
+      api.sendMessage(
+        "Error: " + error.message,
+        event.threadID,
+        event.messageID
+      );
+
+    }
+
+  }
 };
