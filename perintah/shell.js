fc3c000b55c6fcf32b1eb757f6dfabe1dab8b15a
Hady Zen'in <hadyzenin@gmail.com>
2026-05-29 22:52:37 +0700

    Hady Zen'in

--- perintah/shell.js
+++ perintah/shell.js
@@ -1,32 +1,53 @@
 const { exec } = require("child_process");
+
 module.exports = {
   hady: {
-    nama: "shell", 
+    nama: "shell",
     kuldown: 6,
     peran: 2,
-    penulis: "Hady Zen", 
+    penulis: "Hady Zen",
     tutor: "<kode>"
-},
-  
-bahasa: { 
-  id: { hadi: "Kamu belum memasukkan kode terminal nya." }, 
-  en: { hadi: " You haven't entered the terminal code." }
-}, 
-  
-Ayanokoji: async function ({ api, event, args, bhs }) {
-    if (!args.join(' ')) return api.sendMessage(bhs('hadi'), event.threadID, event.messageID);
+  },
+
+  Ayanokoji: async function ({ api, event, args }) {
+
+    if (!args[0]) {
+
+      return api.sendMessage(
+        "Kamu belum memasukkan kode terminal nya.",
+        event.threadID,
+        event.messageID
+      );
+
+    }
+
     exec(args.join(" "), (error, stdout, stderr) => {
+
       let hadi = "";
+
       if (error) {
         hadi = error.message;
       }
+
       if (stdout) {
         hadi = stdout;
       }
+
       if (stderr) {
         hadi = stderr;
       }
-      api.sendMessage(hadi, event.threadID, event.messageID);
+
+      if (!hadi) {
+        hadi = "Tidak ada output.";
+      }
+
+      api.sendMessage(
+        hadi,
+        event.threadID,
+        event.messageID
+      );
+
     });
+
   },
 };
