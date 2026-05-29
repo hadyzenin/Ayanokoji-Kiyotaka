46a78b79ff488c046f8ac41ced35ec9c3a83d774
Hady Zen'in <hadyzenin@gmail.com>
2026-05-29 22:55:36 +0700

    Hady Zen'in

--- perintah/kick.js
+++ perintah/kick.js
@@ -1,29 +1,62 @@
-module.exports = { 
-hady: { 
-  nama: "kick",
-  penulis: "Hady Zen", 
-  kuldown: 10,
-  peran: 1,
-  tutor: "<id/reply/tag>"
-}, 
+module.exports = {
+  hady: {
+    nama: "kick",
+    penulis: "Hady Zen",
+    kuldown: 10,
+    peran: 1,
+    tutor: "<id/reply/tag>"
+  },
 
-bahasa: {
-  id: { hadi: "Kamu belum memasukkan id nya." }, 
-  en: { hadi: "You haven't given her id yet." }
-}, 
-  
-Ayanokoji: async function ({ api, event, args, bhs }) {
-if (event.messageReply) return api.removeUserFromGroup(event.messageReply.senderID, event.threadID);
-if (args[0]) {
-  const { mentions } = event;
-	let hadi = ''; 
-for (const id in mentions) { 
-  hadi += `${mentions[id].replace("@", "")}: ${id}\n`;
-}
-  api.removeUserFromGroup(`${hadi || args[0]}`, event.threadID);
-return;
-} else { 
-  api.sendMessage(bhs('hadi'), event.threadID, event.messageID);
+  Ayanokoji: async function ({ api, event, args }) {
+
+    let target;
+
+    if (event.messageReply) {
+
+      target = event.messageReply.senderID;
+
+    } else if (Object.keys(event.mentions).length > 0) {
+
+      target = Object.keys(event.mentions)[0];
+
+    } else if (args[0]) {
+
+      target = args[0];
+
+    } else {
+
+      return api.sendMessage(
+        "Kamu belum memasukkan id nya.",
+        event.threadID,
+        event.messageID
+      );
+
+    }
+
+    try {
+
+      await api.removeUserFromGroup(
+        target,
+        event.threadID
+      );
+
+      api.sendMessage(
+        "Berhasil mengeluarkan pengguna.",
+        event.threadID,
+        event.messageID
+      );
+
+    } catch (e) {
+
+      console.log(e);
+
+      api.sendMessage(
+        "Gagal mengeluarkan pengguna.",
+        event.threadID,
+        event.messageID
+      );
+
+    }
+
   }
- }
 };
