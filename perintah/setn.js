7c3eb43f918ce5a1a8b9f12aeb889908d9fdb4d3
Hady Zen'in <hadyzenin@gmail.com>
2026-05-29 23:07:58 +0700

    Hady Zen'in

--- perintah/setn.js
+++ perintah/setn.js
@@ -2,34 +2,70 @@ module.exports = {
   hady: {
     nama: "setn",
     penulis: "Hady Zen",
-    kuldown: 10,
+    kuldown: 8,
     peran: 0,
     tutor: "<nama>"
   },
 
-  bahasa: {
-    id: { hady: "Harap masukkan nama yang kamu mau.", 
-          hadi: "Nama kamu lebih dari 12 huruf.", 
-          zen: "Kamu membutuhkan 2 yen untuk mengganti nama.",
-          kiyopon: "Kamu berhasil mengganti nama jadi " }, 
-    en: { hady: "Please enter the name you want.", 
-          hadi: "Your name is more than 12 letters.", 
-          zen: "You need 2 yen to change the name.", 
-          kiyopon: "You managed to change the name to " }
-  }, 
-  
-  Ayanokoji: async function ({ api, event, args, setUser, getData, bhs }) { 
-    const nama = args.join(' ');
-  if (!nama) { return api.sendMessage(bhs("hady"), event.threadID, event.messageID);
-  } 
-  if (nama.length > 12) {
-    return api.sendMessage(bhs("hadi"), event.threadID, event.messageID);
+  Ayanokoji: async function ({
+    api,
+    event,
+    args,
+    setUser,
+    getData
+  }) {
+
+    const nama = args.join(" ");
+
+    if (!nama) {
+
+      return api.sendMessage(
+        "Harap masukkan nama yang kamu mau.",
+        event.threadID,
+        event.messageID
+      );
+
+    }
+
+    if (nama.length > 12) {
+
+      return api.sendMessage(
+        "Nama kamu lebih dari 12 huruf.",
+        event.threadID,
+        event.messageID
+      );
+
+    }
+
+    const { yen } = getData(event.senderID);
+
+    if (yen < 2) {
+
+      return api.sendMessage(
+        "Kamu membutuhkan 2 yen untuk mengganti nama.",
+        event.threadID,
+        event.messageID
+      );
+
+    }
+
+    setUser(
+      event.senderID,
+      "nama",
+      nama
+    );
+
+    setUser(
+      event.senderID,
+      "yen",
+      yen - 2
+    );
+
+    api.sendMessage(
+      `Kamu berhasil mengganti nama jadi ${nama}`,
+      event.threadID,
+      event.messageID
+    );
+
   }
-   const { yen } = getData(event.senderID);
- if (yen < 2) { return api.sendMessage(bhs("zen"), event.threadID, event.messageID);
-}
-  setUser(event.senderID, "nama", nama);
-  setUser(event.senderID, "yen", yen - 2);
-   api.sendMessage(bhs("kiyopon") + nama, event.threadID, event.messageID);
- }, 
 };
