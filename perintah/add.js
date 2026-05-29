bf953add56c13a19d21db3743439d2afd11aed86
Hady Zen'in <hadyzenin@gmail.com>
2026-05-29 22:54:42 +0700

    Hady Zen'in

--- perintah/add.js
+++ perintah/add.js
@@ -1,24 +1,48 @@
 module.exports = {
-hady: { 
-  nama: "add",
-  penulis: "Hady Zen", 
-  peran: 1,
-  kuldown: 10,
-  tutor: "<id>"
-}, 
-  
-bahasa: {
-  id: { hadi: "Kamu belum memberikan id nya." }, 
-  en: { hadi: "You haven't given her id yet." }
-}, 
-  
-Ayanokoji: async function ({ api, event, args, bhs }) { 
- if (!args[0]) return api.sendMessage(bhs("hadi"), event.threadID, event.messageID);
-  
-    try { 
-  api.addUserToGroup(args[0], event.threadID);
-  } catch (e) {
-    console.log(e);
+  hady: {
+    nama: "add",
+    penulis: "Hady Zen",
+    peran: 1,
+    kuldown: 10,
+    tutor: "<id>"
+  },
+
+  Ayanokoji: async function ({ api, event, args }) {
+
+    if (!args[0]) {
+
+      return api.sendMessage(
+        "Kamu belum memberikan id nya.",
+        event.threadID,
+        event.messageID
+      );
+
+    }
+
+    try {
+
+      await api.addUserToGroup(
+        args[0],
+        event.threadID
+      );
+
+      api.sendMessage(
+        "Berhasil menambahkan pengguna.",
+        event.threadID,
+        event.messageID
+      );
+
+    } catch (e) {
+
+      console.log(e);
+
+      api.sendMessage(
+        "Gagal menambahkan pengguna.",
+        event.threadID,
+        event.messageID
+      );
+
+    }
+
   }
- }
 };
