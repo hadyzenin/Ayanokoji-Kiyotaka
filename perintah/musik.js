93422a46dfb3a46dd1d0e6d0c6dbb25f8bb14ab7
Hady Zen'in <hadyzenin@gmail.com>
2026-06-10 17:36:37 +0700

    Hady Zen'in

--- perintah/musik.js
+++ perintah/musik.js
@@ -5,25 +5,27 @@ module.exports = {
   hady: {
     nama: "musik",
     penulis: "Hady Zen",
-    kuldown: 26,
+    kuldown: 30,
     peran: 0,
     tutor: "<judul>"
   },
 
   Ayanokoji: async function ({ api, event, args, getStream }) {
     const query = args.join(" ");
-    if (!query) return api.sendMessage("Berikan saya judul musik nya.", event.threadID, event.messageID);
+
+    if (!query) return api.sendMessage("Masukkan judul musik.", event.threadID, event.messageID);
 
     try {
-      const { data } = await axios.get(`https://api.jamendo.com/v3.0/tracks/?client_id=82684229&format=json&limit=1&search=${encodeURIComponent(query)}`);
-      const musik = data.results[0];
+      const { data } = await axios.get(`https://discoveryprovider.audius.co/v1/tracks/search?query=${encodeURIComponent(query)}`);
+      const musik = data.data[0];
 
       if (!musik) return api.sendMessage("Musik tidak ditemukan.", event.threadID, event.messageID);
 
-      const file = await getStream(musik.audio, "musik.mp3");
+      const audio = `https://discoveryprovider.audius.co/v1/tracks/${musik.id}/stream`;
+      const file = await getStream(audio, "musik.mp3");
 
       api.sendMessage({
-        body: `Judul: ${musik.name}\n Download ${musik.audio}`,
+        body: `Judul: ${musik.title}\n Link: ${audio}`,
         attachment: fs.createReadStream(file)
       }, event.threadID, event.messageID);
 
