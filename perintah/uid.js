e1a75d1246bbecdec5ee99c8339152099583495e
Hady Zen'in <hadyzenin@gmail.com>
2026-07-02 15:44:25 +0700

    Hady Zen'in

--- perintah/uid.js
+++ perintah/uid.js
@@ -1,23 +1,36 @@
 module.exports = {
-  hady: { 
-    nama: "uid", 
-    penulis: "Hady Zen", 
+  hady: {
+    nama: "uid",
+    penulis: "Hady Zen",
     kuldown: 10,
     peran: 0,
-    tutor: "<kosong/reply/tag>"
-  }, 
-  
-  Ayanokoji: async function ({ api, event, args }) {
-if (event.messageReply) return api.sendMessage(event.messageReply.senderID, event.threadID, event.messageID);
-if (!args[0]) return api.sendMessage(event.senderID, event.threadID, event.messageID);
-if (args[0]) {
-  const { mentions } = event;
-	let hadi = ''; 
-for (const id in mentions) { 
-  hadi += `${mentions[id].replace("@", "")}: ${id}\n`;
-}
-  api.sendMessage(hadi, event.threadID, event.messageID);
-return;
-}
- }
+    tutor: "<kosong/reply/tag/link>"
+  },
+
+  Ayanokoji: async function ({ api, event, args, fbid }) {
+    if (event.messageReply) return api.sendMessage(event.messageReply.senderID, event.threadID, event.messageID);
+
+    if (!args[0]) return api.sendMessage(event.senderID, event.threadID, event.messageID);
+
+    const { mentions } = event;
+
+    if (Object.keys(mentions).length) {
+      let hadi = "";
+      for (const id in mentions) hadi += `${mentions[id].replace("@", "")}: ${id}\n`;
+      return api.sendMessage(hadi.trim(), event.threadID, event.messageID);
+    }
+
+    const regExCheckURL = /^(http|https):\/\/[^ "]+$/;
+
+    if (regExCheckURL.test(args[0])) {
+      try {
+        const uid = await fbid(args[0]);
+        return api.sendMessage(uid, event.threadID, event.messageID);
+      } catch (e) {
+        return api.sendMessage(e.message, event.threadID, event.messageID);
+      }
+    }
+
+    api.sendMessage("Masukkan link Facebook yang valid.", event.threadID, event.messageID);
+  }
 };
