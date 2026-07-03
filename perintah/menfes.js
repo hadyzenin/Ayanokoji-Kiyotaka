9735e1a35fa3732de7fdb5373af070f2a34e1d62
Hady Zen'in <hadyzenin@gmail.com>
2026-07-03 23:52:32 +0700

    Hady Zen'in

--- perintah/menfes.js
+++ perintah/menfes.js
@@ -8,12 +8,23 @@ module.exports = {
   },
 
   Ayanokoji: async function ({ api, event, args }) {
-    if (args.length < 2) return api.sendMessage("Contoh:\nmenfes 1000123456789 Halo apa kabar", event.threadID, event.messageID);
+    if (args.length < 2) return api.sendMessage("Contoh: menfes 1000123456789 halo", event.threadID, event.messageID);
 
     const id = args[0];
     const pesan = args.slice(1).join(" ");
 
-    api.sendMessage(`📩 𝗠𝗲𝗻𝗳𝗲𝘀\n\n${pesan}`, id, err => {
+    api.sendMessage(`✦˚₊‧꒰ა MENFES KIYOPON ໒꒱ ‧₊˚✦
+
+୨୧💛 from: human
+୨୧🩵 for: you
+
+┈┈┈୨୧┈┈┈
+
+⏤͟͟͞͞ pesan: ${pesan}
+
+┈┈┈୨୧┈┈┈
+
+jangan lupa dibaca yaa sayangg 💌`, id, err => {
       if (err) return api.sendMessage("Gagal mengirim menfes.", event.threadID, event.messageID);
       api.sendMessage("Menfes berhasil dikirim.", event.threadID, event.messageID);
     });
