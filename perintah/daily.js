578915724e75bc5110af6bd8e11f06d93f475b4f
Hady Zen'in <hadyzenin@gmail.com>
2026-07-04 21:44:13 +0700

    Hady Zen'in

--- perintah/daily.js
+++ perintah/daily.js
@@ -11,11 +11,11 @@ module.exports = {
   const { yen, exp, daily } = getData(event.senderID);
     
     if (daily == null || daily !== global.Ayanokoji.tanggal) { 
-  const duit = Math.ceil(Math.random() * 10);
+  const duit = Math.ceil(Math.random() * 20);
   setUser(event.senderID, 'yen', yen + duit);
-  setUser(event.senderID, 'exp', exp + 10);
+  setUser(event.senderID, 'exp', exp + 80);
   setUser(event.senderID, 'daily', global.Ayanokoji.tanggal);
-    api.sendMessage(`Kamu berhasil mengklaim ${duit} yen dan 10 exp`, event.threadID, event.messageID);
+    api.sendMessage(`Kamu berhasil mengklaim ${duit} yen dan 80 exp`, event.threadID, event.messageID);
     } else {
     api.sendMessage('Kamu sudah mengklaim hadiah harian.', event.threadID, event.messageID);
   }
