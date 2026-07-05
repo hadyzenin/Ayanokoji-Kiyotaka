d6a5ec2680e1ca59590f95a69741243ff93107d6
Hady Zen'in <hadyzenin@gmail.com>
2026-07-06 00:50:37 +0700

    Hady Zen'in

--- hady-zen/func.js
+++ hady-zen/func.js
@@ -4,6 +4,7 @@
  const fs = require('fs');
  const path = require('path');
  const axios = require('axios');
+ const cheerio = require('cheerio');
 
 async function getStream(hadi, isekai) {
     try {
